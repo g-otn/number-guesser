f034f01bf877fc6b3fe8b39e9a23a121e84c0e3a
g-otn <g-otn@hotmail.com>
2020-11-11 01:38:25 -0300

    refactor: use CrossValidation and update model and view style/layout

--- train/train.js
+++ train/train.js
@@ -4,27 +4,31 @@ const fs = require('fs');
 
 
 // Get training data
-const set = MNIST.set(45000, 15000); // Get 60000 digits
+const set = MNIST.set(45000, 15000); // Get training and testing sets
 // const set = MNIST.set(8000, 2000);
-const trainingData = [...set.training, ...set.test]; // No test is being made (not using brain.CrossValidation), so we can use the training and test sets as training data
+const trainingData = [...set.training, ...set.test]; // brain.CrossValidation will divide the data set into training and testing internally, so we send them altogether
 
 
 // Structure and create neural network
-const net = new brain.NeuralNetwork({
+const netOptions = {
+  iterations: 20000,
   hiddenLayers: [16, 16],
   activation: 'sigmoid'
-});
+};
+// const net = new brain.NeuralNetwork(netOptions);
+const crossValidate = new brain.CrossValidate(brain.NeuralNetwork, netOptions); 
 
 
-// Train network with MNIST digits
+// Train neural network with MNIST digits
 console.log('Training with', trainingData.length, 'images');
-net.train(trainingData, {
+crossValidate.train(trainingData, {
+  learningRate: 0.1,
   log: true,
   logPeriod: 1
 });
 
 
 // Export neural network model to file
-const model = net.toJSON();
+const model = crossValidate.toNeuralNetwork().toJSON();
 const modelFilePath = '../client/model.json';
 fs.writeFile(modelFilePath, JSON.stringify(model), 'utf8', () => console.log('Model exported to ' + modelFilePath));
