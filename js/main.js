973be05f3137d2fec063534c614fbb0c102eea42
g-otn <g-otn@hotmail.com>
2020-11-12 04:39:08 -0300

    use modified script and display output layer values with html

--- js/main.js
+++ js/main.js
@@ -9,11 +9,10 @@
   }
 
   function createVisualizationDigitPad($element) {
-    console.log($element)
     return new DigitPad($element, {
       columns: 28,
       rows: 28,
-      cellSize: 2,
+      cellSize: 2.75,
       disablePad: true
     });
   }
@@ -23,29 +22,53 @@
     // for (let i = 0; i < node.weights.length)
   }
 
+  function showOutput(output) { // Display output layer values
+    console.log('Showing output', output)
+    // let test = {};
+    // for (let key in output) {
+    //   if (output.hasOwnProperty(key)) {
+    //     test[key] = Math.floor(output[key] * 10000) / 100; // round percentages
+    //     test[key] += '%'
+    //   }
+    // }
+    // $('#test').text(JSON.stringify(test, null, '\t'));
+    $('#test').html('')
+    // const result = $('<div></div>');
+    output.forEach((v, i) => {
+      const valueText = (Math.floor(v * 10000) / 100).toFixed(2) + '%';
+      // const template = $($('#outputTemplate').html())
+      // template.text(`${i} ${valueText}`);
+      $('#test').append(`<br>&nbsp;${i}:&nbsp;&nbsp;&nbsp;${valueText}`)
+      // result.append(template);
+    });
+  }
+  
+
   $(document).ready(async () => {
   
-    // Structure neural network and load model
+    // Structure neural network
     const net = new brain.NeuralNetwork(brain.NeuralNetwork, {
       hiddenLayers: [16, 16],
       activation: 'sigmoid'
     });
     
+
+    // Load model from JSON file (created from Node.js side training)
     const model = await fetch('model.json').then(res => res.json());
     net.fromJSON(model);
+    console.log('Loaded model:', model);
 
 
     // Initialize first hidden layer visualization grids
-    // console.log('Model sizes:', model.sizes);
-    // for (let i = 0; i < model.sizes[1]; i++) {
-    //   $('#visualizationGrids').append('<div class="digit-pad"></div>');
+    for (let node = 0; node < model.sizes[1]; node++) {
+      $('#visualizationGrids').append('<div class="digit-pad"></div>');
       
-    //   const $padContainer = $('#visualizationGrids .digit-pad')[i];
+      const $padContainer = $('#visualizationGrids .digit-pad')[node];
       
-    //   const pad = createVisualizationDigitPad($padContainer);
-    //   pad.init();
+      const pad = createVisualizationDigitPad($padContainer, model.layers[1][node]);
+      pad.init();
       
-    // }
+    }
 
 
     // Initialize digit pad
@@ -67,14 +90,9 @@
         const prediction = net.run(input);
         console.info('Prediction:', prediction);
 
-        // Test - display prediction
-        let test = {};
-        for (let key in prediction) {
-          if (prediction.hasOwnProperty(key)) {
-            test[key] = Math.floor(prediction[key] * 100000) / 1000; // round percentages
-          }
-        }
-        $('#test').text(JSON.stringify(test, null, '\t'));
+        // Display prediction
+        const outputLayerIndex = model.sizes.length - 1;
+        showOutput(prediction[outputLayerIndex]);
       }
     });
     digitPad.init();
