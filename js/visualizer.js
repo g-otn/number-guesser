051fc0a3d2c584be51312c13bb4cf22154bb730f
g-otn <g-otn@hotmail.com>
2020-11-19 02:05:05 -0300

    feat: input 3d visualizer

--- js/visualizer.js
+++ js/visualizer.js
@@ -7,6 +7,8 @@
   function Visualizer($element) {
     this.raycaster = new THREE.Raycaster();
     this.mouse = new THREE.Vector2(); // create once
+    this.model = {};
+    this.inputCells = [];
 
     this.init = () => {
       // const width = window.innerWidth, height = window.innerHeight;
@@ -22,6 +24,7 @@
 
       // Scene
       this.scene = new THREE.Scene();
+      this.scene.background = new THREE.Color(`rgb(5, 5, 5)`);
 
       // Camera
       this.camera = new THREE.PerspectiveCamera(40, width / height, 1, 10000);
@@ -31,44 +34,76 @@
       CameraControls.install({ THREE: THREE });
       this.clock = new THREE.Clock();
       this.camera = new THREE.PerspectiveCamera(40, width / height, 1, 10000);
-      this.camera.position.set(20, 20, 20);
+      // this.camera.position.set(280 / 2, 280 / 2, 500);
+      // this.camera.lookAt(new THREE.Vector3(80 / 2, 280 / 2, 500));
       this.cameraControls = new CameraControls(this.camera, this.renderer.domElement);
+      this.cameraControls.setLookAt(
+        -800, 280/2, -150,              // Position (to look from)
+        280 / 2, 280 / 2, 300         // Target/Focus (to look at)
+      )
 
       // Ambient light
-      this.scene.add(new THREE.AmbientLight(0x555555));
+      this.scene.add(new THREE.AmbientLight(0xffffff));
       // Directional light
-      var light = new THREE.DirectionalLight(0xffffff, 1);
-      light.position.set(20, 20, 0);
-      this.scene.add(light);
+      // var light = new THREE.DirectionalLight(0xffffff, 1);
+      // light.position.set(20, 20, 0);
+      // this.scene.add(light);
 
       // Axes
-      this.scene.add(new THREE.AxesHelper(200));
+      this.scene.add(new THREE.AxesHelper(1000));
 
-      // Geometry
-      const geometry = new THREE.SphereGeometry(10, 20, 10);
-      // material
-      const material = new THREE.MeshPhongMaterial({
-        color: 0x33bbff,
-        flatShading: true,
-        transparent: true,
-        opacity: 0.7,
-      });
-      // testMesh
-      mesh = new THREE.Mesh(geometry, material);
-      this.scene.add(mesh);
+      // Test mesh
+      // const geometry = new THREE.SphereGeometry(10, 20, 10);
+      // const material = new THREE.MeshPhongMaterial({
+      //   color: 0x33bbff,
+      //   flatShading: true,
+      //   transparent: true,
+      //   opacity: 0.7,
+      // });
+      // mesh = new THREE.Mesh(geometry, material);
+      // this.scene.add(mesh);
 
 
       // Input layer meshes
-      for (let i = 0; i < 28; i++) {
-        const inputGeometry = new THREE.PlaneBufferGeometry(10, 10);
-        
+      const cellSize = 10;
+      this.inputCells = [];
+      for (let y = 0; y < 28; y++) {
+        this.inputCells[y] = [];
+        for (let x = 0; x < 28; x++) {
+          // Create mesh
+          const inputGeometry = new THREE.PlaneBufferGeometry(cellSize, cellSize);
+          const inputMaterial = new THREE.MeshBasicMaterial({
+            color: 0x000000,
+            transparent: true,
+            opacity: 0.9,
+            side: THREE.DoubleSide
+          })
+          const inputCell = new THREE.Mesh(inputGeometry, inputMaterial);
+          inputCell.position.set(
+            (cellSize * 28 - x * cellSize), 
+            (cellSize * 28 - y * cellSize), 
+            0
+          );
+
+          // Add custom data to mesh
+          inputCell.userData = {
+            layer: 0,
+            row: y,
+            column: x
+          }
+
+          this.inputCells[y][x] = inputCell;
+
+          this.scene.add(inputCell)
+          // console.log(inputCell.material.color)
+        }
       }
 
 
 
       // Mouse interaction
       this.renderer.domElement.addEventListener('click', e => {
-        // Calculate mouse 2D position based on click and canvas position on document (accounting scroll)
+        // Calculate mouse 2D position on canvas based on mouse click and canvas position on document (accounting document scroll)
         const DOMRect = this.renderer.domElement.getBoundingClientRect();
         this.mouse.x = ((e.clientX - DOMRect.left) / this.renderer.domElement.clientWidth) * 2 - 1;
         this.mouse.y = - ((e.clientY - DOMRect.top) / this.renderer.domElement.clientHeight) * 2 + 1;
@@ -81,6 +116,11 @@
         if (intersects.length > 0) {
           console.log('Intersection at', this.mouse, intersects[0]);
 
+          const userData = intersects[0].object.userData;
+          console.log('userData:', userData);
+
+          this.inputCells[userData.row][userData.column].material.color.setHex(0x0000aa)
+
           // Test (draw line)
           // const material = new THREE.LineBasicMaterial( { color: 0x00ccff } );
           // const geometry = new THREE.BufferGeometry().setFromPoints([
@@ -89,7 +129,7 @@
           // ]);
           // const line = new THREE.Line( geometry, material );
           // this.scene.add( line );
-          
+
           // Test (draw cube)
           const geometry = new THREE.SphereGeometry(1, 40, 40);
           const material = new THREE.MeshPhongMaterial({ color: 0x00eecc, transparent: true, opacity: 0.4, });
@@ -111,8 +151,24 @@
       this.renderer.render(this.scene, this.camera);
     }
 
-    this.updateInput = (grid) => {
+    this.loadModel = (model) => {
+      this.model = model;
 
+      // Load hidden and output layers
+      for (let l = 1; l < model.layers.length; l++) {
+
+      }
+    }
+
+    this.updateInputCells = (grid) => {
+      // inputCell.material.color.setHex(0xddffcc) // setRGB(,,)
+      for (let y = 0; y < 28; y++) {
+        for (let x = 0; x < 28; x++) {
+          const grayValue = grid[y][x];
+          this.inputCells[y][x].material.color.setRGB(grayValue, grayValue, grayValue);
+          this.inputCells[y][x].userData.value = grid[y][x];
+        }
+      }
     }
   }
 
